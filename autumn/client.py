c753e69f009e29d137df47631c9adeb298a66dff
justanotherbyte <wishymovies@gmail.com>
2025-06-05 23:22:57 +0100

    properly handle product_id and product_ids. Only send product_ids

--- autumn/client.py
+++ autumn/client.py
@@ -25,8 +25,8 @@ class Client:
     def attach(
         self,
         customer_id: str,
-        product_id: str,
         *,
+        product_id: Optional[str] = None,
         product_ids: Optional[List[str]] = None,
         success_url: Optional[str] = None,
         force_checkout: bool = False,
@@ -34,7 +34,17 @@ class Client:
         entity_id: Optional[str] = None,
         customer_data: Optional[Customer] = None,
     ) -> AttachResponse:
-        payload = _build_payload(locals(), self.attach)
+        assert product_id is not None or product_ids is not None, (
+            "Either product_id or product_ids must be provided"
+        )
+        assert not (product_id is not None and product_ids is not None), (
+            "Only one of product_id or product_ids must be provided"
+        )
+
+        if product_id is not None:
+            product_ids = [product_id]
+
+        payload = _build_payload(locals(), self.attach, ignore={"product_id"})
         return self.http.request("POST", "/attach", AttachResponse, json=payload)
 
     def check(
