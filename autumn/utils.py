fadb3e21c3da1cfbf2b5f9c280b5925f5606f840
justanotherbyte <wishymovies@gmail.com>
2025-06-05 21:20:28 +0100

    use model_dump() instead of model_dump_json()

---
+++ autumn/utils.py
@@ -0,0 +1,34 @@
+from typing import Dict, Any, Callable, Type, TypeVar
+
+from pydantic import BaseModel, ValidationError
+
+from .error import AutumnValidationError
+
+
+T = TypeVar("T", bound=BaseModel)
+
+
+def _build_payload(scope: Dict[str, Any], method: Callable) -> Dict[str, Any]:
+    params = method.__code__.co_varnames
+    payload: Dict[str, Any] = {}
+
+    for key, value in scope.items():
+        if key != "self" and key in params:
+            payload[key] = value.model_dump() if isinstance(value, BaseModel) else value
+
+    return payload
+
+
+def _build_model(model: Type[T], data: Dict[str, Any]) -> T:
+    try:
+        return model.model_validate(data)
+    except ValidationError as e:
+        errors = e.errors()
+        error_message = errors[0]["msg"]
+        error_path = errors[0]["loc"]
+        error_code = errors[0]["type"]
+
+        raise AutumnValidationError(
+            f"{error_message} at {error_path} with code {error_code}",
+            "validation_error",
+        )
