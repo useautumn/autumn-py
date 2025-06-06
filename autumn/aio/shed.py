9b3cc22f05fa675d170ecaf7559d55a7cddd565a
justanotherbyte <wishymovies@gmail.com>
2025-06-06 10:29:18 +0100

    update async protocols

--- autumn/aio/shed.py
+++ autumn/aio/shed.py
@@ -1,51 +1,65 @@
-import typing
+from __future__ import annotations
 
-from ..types import Feature, Customer, AttachResponse, CheckResponse, TrackResponse
+from typing import TYPE_CHECKING, Protocol
+
+if TYPE_CHECKING:
+    from typing import Optional, List, Awaitable, Dict, Any
+
+    from ..types.meta import AttachOption
+    from ..types.features import Feature
+    from ..types.customers import Customer
+    from ..types.response import (
+        AttachResponse,
+        CheckResponse,
+        TrackResponse,
+    )
 
 
 __all__ = ("AttachParams", "CheckParams", "TrackParams")
 
 
-class AttachParams(typing.Protocol):
+class AttachParams(Protocol):
     def __call__(
         self,
         customer_id: str,
-        product_id: str,
         *,
-        product_ids: typing.Optional[typing.List[str]] = None,
-        success_url: typing.Optional[str] = None,
+        product_id: Optional[str] = None,
+        product_ids: Optional[List[str]] = None,
+        success_url: Optional[str] = None,
         force_checkout: bool = False,
-        features: typing.Optional[typing.List[Feature]] = None,
-        entity_id: typing.Optional[str] = None,
-        customer_data: typing.Optional[Customer] = None,
-    ) -> typing.Awaitable[AttachResponse]: ...
+        features: Optional[List[Feature]] = None,
+        entity_id: Optional[str] = None,
+        customer_data: Optional[Customer] = None,
+        free_trial: Optional[bool] = None,
+        options: Optional[List[AttachOption]] = None,
+    ) -> Awaitable[AttachResponse]: ...
 
 
-class CheckParams(typing.Protocol):
+class CheckParams(Protocol):
     def __call__(
         self,
         customer_id: str,
         *,
-        product_id: typing.Optional[str] = None,
-        feature_id: typing.Optional[str] = None,
-        required_balance: typing.Optional[int] = 1,
+        product_id: Optional[str] = None,
+        feature_id: Optional[str] = None,
+        required_balance: Optional[int] = 1,
         send_event: bool = False,
         with_preview: bool = False,
-        entity_id: typing.Optional[str] = None,
-        customer_data: typing.Optional[Customer] = None,
-    ) -> typing.Awaitable[CheckResponse]: ...
+        entity_id: Optional[str] = None,
+        customer_data: Optional[Customer] = None,
+    ) -> Awaitable[CheckResponse]: ...
 
 
-class TrackParams(typing.Protocol):
+class TrackParams(Protocol):
     def __call__(
         self,
         customer_id: str,
         feature_id: str,
         *,
         value: int = 1,
-        entity_id: typing.Optional[str] = None,
-        event_name: typing.Optional[str] = None,
-        idempotency_key: typing.Optional[str] = None,
-        properties: typing.Optional[typing.Dict[str, typing.Any]] = None,
-        customer_data: typing.Optional[Customer] = None,
-    ) -> typing.Awaitable[TrackResponse]: ...
+        entity_id: Optional[str] = None,
+        event_name: Optional[str] = None,
+        idempotency_key: Optional[str] = None,
+        properties: Optional[Dict[str, Any]] = None,
+        customer_data: Optional[Customer] = None,
+    ) -> Awaitable[TrackResponse]: ...
