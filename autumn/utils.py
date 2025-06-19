1dc1789aac477384073ac22aa48908754e0c2317
justanotherbyte <wishymovies@gmail.com>
2025-06-19 22:13:32 +0100

    fix: ignore None values when building payloads

--- autumn/utils.py
+++ autumn/utils.py
@@ -24,7 +24,7 @@ def _build_payload(
     payload: Dict[str, Any] = {}
 
     for key, value in scope.items():
-        if key != "self" and key in params and key not in ignore:
+        if key != "self" and key in params and key not in ignore and value is not None:
             payload[key] = _decompose_value(value)
 
     return payload
