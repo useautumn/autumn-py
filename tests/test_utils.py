8c500a54518d66266a79d02fe70d52ad0463153d
justanotherbyte <wishymovies@gmail.com>
2025-06-19 14:13:30 +0100

    autumn/utils.py tests + add pytest suite as a depednency

---
+++ tests/test_utils.py
@@ -0,0 +1,87 @@
+import pytest
+
+from autumn.types.customers import CustomerFeature, ProductItemInterval
+from autumn.customers import Customers
+from autumn.error import AutumnValidationError
+from autumn.utils import _build_model, _build_payload, _decompose_value
+
+
+def test_decompose_value_singular():
+    feature = CustomerFeature(
+        id="mock_id",
+        name="John Doe",
+        unlimited=False,
+        interval=ProductItemInterval.HOUR,
+    )
+    decomposed_feature = {
+        "id": "mock_id",
+        "name": "John Doe",
+        "unlimited": False,
+        "interval": "hour",
+        "balance": None,
+        "usage": None,
+        "included_usage": None,
+        "next_reset_at": None,
+        "breakdown": None,
+    }
+
+    assert _decompose_value(feature) == decomposed_feature
+
+
+def test_decompose_value_recursive():
+    feature = CustomerFeature(
+        id="mock_id",
+        name="John Doe",
+        unlimited=False,
+        interval=ProductItemInterval.HOUR,
+    )
+    decomposed_feature = {
+        "id": "mock_id",
+        "name": "John Doe",
+        "unlimited": False,
+        "interval": "hour",
+        "balance": None,
+        "usage": None,
+        "included_usage": None,
+        "next_reset_at": None,
+        "breakdown": None,
+    }
+
+    features = [feature] * 50
+    decomposed_features = [decomposed_feature] * 50
+
+    assert _decompose_value(features) == decomposed_features
+
+
+def test_build_payload():
+    mock_scope = {
+        "self": None,
+        "id": "mock_customer_id",
+        "email": "johndoe@example.com",
+        "name": "John Doe",
+        **locals(),
+    }
+    expected_payload = {
+        "id": "mock_customer_id",
+        "email": "johndoe@example.com",
+        "name": "John Doe",
+    }
+
+    assert _build_payload(mock_scope, Customers.create) == expected_payload
+
+
+def test_model_build_exc():
+    bad_data = {
+        "id": "mock_id",
+        "name": "John Doe",
+        "unlimited": False,
+        "interval": "chocolate",
+        "balance": "",
+        "usage": None,
+        "included_usage": None,
+        "next_reset_at": None,
+        "breakdown": None,
+    }
+
+    with pytest.raises(AutumnValidationError):
+        _build_model(CustomerFeature, bad_data)
