320b275b6d7ca6bbbd146cfbe11aa309ff0954ae
justanotherbyte <wishymovies@gmail.com>
2025-06-20 16:54:01 +0100

    add routes

--- autumn/asgi/app.py
+++ autumn/asgi/app.py
@@ -1,28 +1,53 @@
+from __future__ import annotations
+
+from typing import Awaitable, Callable, TypedDict, TYPE_CHECKING
 import contextlib
-import posixpath
 
 from starlette.routing import Router, Route
 
-from .routes import attach_route, check_route, track_route
+from .routes import (
+    attach_route,
+    check_route,
+    track_route,
+    cancel_route,
+    billing_portal_route,
+)
 from ..aio.client import AsyncClient
+
+if TYPE_CHECKING:
+    from starlette.requests import Request
+
+
+class _CustomerData(TypedDict):
+    name: str
+    email: str
+
+
+class AutumnData(TypedDict):
+    customer_id: str
+    customer_data: _CustomerData
 
 
 class AutumnASGI:
-    def __init__(self, *, token: str, base_path: str = "/api/autumn"):
+    def __init__(
+        self,
+        token: str,
+        *,
+        identify: Callable[[Request], Awaitable[AutumnData]],
+    ):
         @contextlib.asynccontextmanager
         async def lifespan(_):
             async with AsyncClient(token=token) as client:
-                yield {"autumn": client}
-
-        def _(path: str):
-            return posixpath.join(base_path, path.lstrip("/"))
+                yield {"autumn": client, "identify": identify}
 
         router = Router(
             lifespan=lifespan,
             routes=[
-                Route(_("/attach"), attach_route, methods={"POST"}),
-                Route(_("/check"), check_route, methods={"POST"}),
-                Route(_("/track"), track_route, methods={"POST"}),
+                Route("/attach", attach_route, methods={"POST"}),
+                Route("/check", check_route, methods={"POST"}),
+                Route("/track", track_route, methods={"POST"}),
+                Route("/cancel", cancel_route, methods={"POST"}),
+                Route("/billing_portal", billing_portal_route, methods={"POST"}),
             ],
         )
         self._router = router
--- autumn/asgi/routes.py
+++ autumn/asgi/routes.py
@@ -10,24 +10,61 @@ if TYPE_CHECKING:
 
 
 async def attach_route(request: Request):
-    data = await request.json()
+    identify = await request.state.identify(request)
+    json = await request.json()
     autumn: AsyncClient = request.state.autumn
 
-    response = await autumn.attach(**data)
+    response = await autumn.attach(**identify, **json)
     return JSONResponse(response.model_dump(mode="json"))
 
 
 async def check_route(request: Request):
-    data = await request.json()
+    identify = await request.state.identify(request)
+    json = await request.json()
     autumn: AsyncClient = request.state.autumn
 
-    response = await autumn.check(**data)
+    response = await autumn.check(**identify, **json)
     return JSONResponse(response.model_dump(mode="json"))
 
 
 async def track_route(request: Request):
-    data = await request.json()
+    identify = await request.state.identify(request)
+    json = await request.json()
     autumn: AsyncClient = request.state.autumn
 
-    response = await autumn.track(**data)
+    response = await autumn.track(**identify, **json)
+    return JSONResponse(response.model_dump(mode="json"))
+
+
+async def cancel_route(request: Request):
+    identify = await request.state.identify(request)
+    json = await request.json()
+    autumn: AsyncClient = request.state.autumn
+
+    response = await autumn.products.cancel_product(
+        customer_id=identify["customer_id"], **json
+    )
+    return JSONResponse(response.model_dump(mode="json"))
+
+
+async def billing_portal_route(request: Request):
+    identify = await request.state.identify(request)
+    json = await request.json()
+    autumn: AsyncClient = request.state.autumn
+
+    response = await autumn.customers.get_billing_portal(
+        customer_id=identify["customer_id"], **json
+    )
+    return JSONResponse(response.model_dump(mode="json"))
+
+
+async def create_entity_route(request: Request):
+    identify = await request.state.identify(request)
+    json = await request.json()
+    autumn: AsyncClient = request.state.autumn
+
+    response = await autumn.features.create_entity(
+        customer_id=identify["customer_id"], **json
+    )
+
     return JSONResponse(response.model_dump(mode="json"))
