58a39bd0ba7d4a85daac37a36b14634693162c36
justanotherbyte <wishymovies@gmail.com>
2025-06-22 00:07:25 +0100

    add delete entity route

--- autumn/asgi/routes/entities.py
+++ autumn/asgi/routes/entities.py
@@ -17,3 +17,13 @@ async def create_entity_route(request: Request):
     response = await autumn.features.create_entity(customer_id=customer_id, **json)
 
     return JSONResponse(response.model_dump(mode="json"))
+
+
+async def delete_entity(request: Request):
+    identify, autumn, json = await _extract(request)
+
+    customer_id = identify["customer_id"]
+    entity_id = json["identity_id"]
+    response = await autumn.features.delete_entity(customer_id, entity_id)
+
+    return JSONResponse(response.model_dump(mode="json"))
