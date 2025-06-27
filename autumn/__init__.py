360b0a53489898576fdc3fa0196123665cedca01
justanotherbyte <wishymovies@gmail.com>
2025-06-27 18:09:21 +0100

    chore: bump version to 1.3.1

--- autumn/__init__.py
+++ autumn/__init__.py
@@ -2,7 +2,7 @@ from .client import *  # noqa: F403
 from .error import *  # noqa: F403
 from .aio.client import AsyncClient as Autumn  # noqa: F401
 
-__version__ = "1.2.1"
+__version__ = "1.3.1"
 __license__ = "MIT"
 __author__ = "justanotherbyte"
 __copyright__ = "Copyright 2025 justanotherbyte"
