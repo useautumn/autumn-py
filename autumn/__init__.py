76c33d14173a67e094d4abf9317bb1ab434bfa0e
justanotherbyte <wishymovies@gmail.com>
2025-07-23 20:54:39 +0530

    chore: bump to 1.5.1

--- autumn/__init__.py
+++ autumn/__init__.py
@@ -8,7 +8,7 @@ from .models.products import *
 from .aio.client import AsyncClient as Autumn
 
 
-__version__ = "1.5.0"
+__version__ = "1.5.1"
 __license__ = "MIT"
 __author__ = "justanotherbyte"
 __copyright__ = "Copyright 2025 justanotherbyte"
