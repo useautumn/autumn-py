9a0f07046cf25829b178f1347093d00b8161724b
justanotherbyte <wishymovies@gmail.com>
2025-08-09 14:36:48 +0530

    docs: use autumn code theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -43,7 +43,7 @@ exclude_patterns = []
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "furo"
-html_static_path = ["_static"]
+html_static_path = []
 html_theme_options = {}
 
 rst_prolog: str = """
@@ -72,4 +72,4 @@ resource_links = {
 
 autodoc_typehints_description_target = "implementation"
 
-pygments_style = "friendly"
+pygments_style = "autumn"
